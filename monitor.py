d7ca7f08be5cfc9a6a75031969e527c76c4e9742
mattia gasparini <mattiagasparini2@gmail.com>
2020-11-25 23:22:59 +0100

    update logger

--- monitor.py
+++ monitor.py
@@ -9,7 +9,7 @@ import csv
 
 class Monitor:
 
-    def __init__(self, url_file, to_date, mongourl='mongodb://localhost:27017/'):
+    def __init__(self, url_file, from_date, mongourl='mongodb://localhost:27017/'):
 
         # load urls file
         with open(url_file, 'r')as furl:
@@ -18,17 +18,14 @@ class Monitor:
         # define MongoDB connection
         self.client = MongoClient(mongourl)
 
-        # max date review to scrape
-        to_date = min(datetime.strptime(to_date, '%Y-%m-%d'), datetime.today())
-        self.max_date_review = to_date
-        print(self.max_date_review)
+        # min date review to scrape
+        self.min_date_review = from_date
+        print(self.min_date_review)
 
         # logging
         self.logger = self.__get_logger()
 
     def scrape_gm_reviews(self):
-
-        self.logger.info('GOOGLEMAPS')
 
         # set connection to DB
         collection = self.client['google-maps']['review']
@@ -59,8 +56,8 @@ class Monitor:
                         # log total number
                         self.logger.info('{} : {} new reviews'.format(url, n_new_reviews))
                     else:
-                        self.logger.warning(
-                            'Sorting reviews failed for {}'.format(url))
+                        self.logger.warning('Sorting reviews failed for {}'.format(url))
+
                 except Exception as e:
                     exc_type, exc_obj, exc_tb = sys.exc_info()
                     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -77,21 +74,18 @@ class Monitor:
 
     def __get_logger(self):
         # create logger
-        logger = logging.getLogger('review-step')
+        logger = logging.getLogger('monitor')
         logger.setLevel(logging.DEBUG)
-        log_path = 'log/review.log'
-        if not os.path.isdir('log/'):
-            os.mkdir('log/')
         # create console handler and set level to debug
-        fh = logging.FileHandler(log_path)
+        fh = logging.FileHandler('monitor.log')
         fh.setLevel(logging.DEBUG)
         # create formatter
-        formatter = logging.Formatter(
-            '%(asctime)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         # add formatter to ch
         fh.setFormatter(formatter)
         # add ch to logger
         logger.addHandler(fh)
+        
         return logger
 
 
@@ -99,11 +93,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Monitor Google Maps places')
     parser.add_argument('--i', type=str, default='urls.txt', help='target URLs file')
+    parser.add_argument('--from_date', type=str) # start date in format: YYYY-MM-DD
 
-    # end date in format: YYYY-MM-DD
-    parser.add_argument('--to_date', type=str, default=datetime.today())
-
-    monitor = Monitor(args.i, args.to_date)
+    monitor = Monitor(args.i, args.from_date)
 
     try:
         monitor.scrape_gm_reviews()
