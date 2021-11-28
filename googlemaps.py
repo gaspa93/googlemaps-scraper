5102b97d99800bcf47ddacdfc8b0fea095474ac1
mattia gasparini <mattiagasparini2@gmail.com>
2021-11-28 23:06:57 +0100

    click on privacy page before starting

--- googlemaps.py
+++ googlemaps.py
@@ -232,12 +232,9 @@ class GoogleMapsScraper:
 
         # first lets click on google agree button so we can continue
         try:
-            google_maps_url = 'https://www.google.com/maps/'
-            input_driver.get(google_maps_url)
-            WebDriverWait(input_driver, 10).until(
-                EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="consent-bump"]/div/div[1]/iframe')))
+            input_driver.get(GM_WEBPAGE)
             agree = WebDriverWait(input_driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, '//*[@id="introAgreeButton"]/span/span')))
+                EC.element_to_be_clickable((By.XPATH, '//span[contains(text(),"I agree")]')))
             agree.click()
 
             # back to the main page
